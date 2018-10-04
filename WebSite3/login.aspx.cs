d475785d433851d5e2bbd4366ceb42fc0aa3e5b4
osOmer <omer_sayem@yahoo.com>
2018-10-04 14:38:19 +0600

    Add files via upload

---
+++ WebSite3/login.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        DataSetTableAdapters.userInfoTableAdapter adapter = new DataSetTableAdapters.userInfoTableAdapter();
+
+        DataSet.userInfoDataTable table = adapter.GetData();
+
+        foreach (DataSet.userInfoRow row in table)
+        {
+            Response.Write(row[1] + "</br>");
+        }
+
+    }
+    
+}
